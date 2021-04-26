1c05b2d593f1fdc1de49ab8c4f4e54177eeaea68
OctaveC <octavec@outlook.com>
2021-04-26 12:16:37 +0000

    first draft first ten exos

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * check_cycle - Finds out if there's a loop in a linked list.
+ * @list: The head of the list we're going to search
+ *
+ * Return: 1 if loop or 0 if no loop
+ */
+int check_cycle(listint_t *list)
+{
+
+	listint_t *search1, *search2;
+
+	search1 = list;
+
+	if (list == NULL)
+		return (0);
+
+	while (search1 != NULL)
+	{
+		if (search1 == search1->next)
+			return (1);
+		for (search2 = list; search2 != search1; search2 = search2->next)
+		{
+			if (search2 == search1->next)
+				return (1);
+		}
+		search1 = search1->next;
+	}
+	return (0);
+}
