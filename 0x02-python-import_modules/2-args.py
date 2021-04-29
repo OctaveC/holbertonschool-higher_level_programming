910bf0cf8eb8e10c3dd6b17feb6136a42042332f
OctaveC <octavec@outlook.com>
2021-04-29 14:57:29 +0000

    third exo done

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+for argv in range(len(sys.argv)):
+    if (argv == 0):
+        if (len(sys.argv) == 1):
+            print("0 arguments.")
+        elif (len(sys.argv) == 2):
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
+    else:
+        print("{}: {}".format(argv, sys.argv[argv]))
