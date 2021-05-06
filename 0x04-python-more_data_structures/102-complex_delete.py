004e581374e76a2ea4d73e7677fe6c753f041584
OctaveC <octavec@outlook.com>
2021-05-06 16:47:55 +0000

    typo

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,5 +2,5 @@
 def complex_delete(a_dictionary, value):
     for ite in list(a_dictionary.keys()):
         if a_dictionary[ite] == value:
-            del(a_dictionary[x])
+            del(a_dictionary[ite])
     return a_dictionary
