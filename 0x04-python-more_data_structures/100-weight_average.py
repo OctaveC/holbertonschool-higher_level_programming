b81b3c036df482d7c368877bbf6bc23de9d6a0a1
OctaveC <octavec@outlook.com>
2021-05-06 16:46:51 +0000

    return 0 not None

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if not my_list:
-        return None
+        return 0
     weight, total = 0, 0
     for (ite1, ite2) in my_list:
         weight += ite2
