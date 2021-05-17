a04c8bdfd4289fb40a00c687c71da7f7359aafbe
OctaveC <octavec@outlook.com>
2021-05-17 14:05:08 +0000

    checker not passing, testing some stuff

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     res_list = []
-    res = 0
     for ite in range(list_length):
         try:
             res = my_list_1[ite] / my_list_2[ite]
         except TypeError:
             res = 0
             print("wrong type")
-        except ZeroDivisionError:
+        except ArithmeticError:
             res = 0
             print("divison by 0")
         except IndexError:
