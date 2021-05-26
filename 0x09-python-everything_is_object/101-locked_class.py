894c466f699b0af5a808a047557cf253eaf5ebcb
OctaveC <octavec@outlook.com>
2021-05-26 18:20:15 +0000

    small pep8 mistake

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Module containing the LockedClass class """
+
+
 class LockedClass:
     """ Class that prevents the user from creating new instance attributes, """
     """ except if the new instance attribute is called first_name. """
