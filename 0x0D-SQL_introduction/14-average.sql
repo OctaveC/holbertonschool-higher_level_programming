5b7b200c6fc01cf2bed6ffb1919f118c18ff11bf
OctaveC <octavec@outlook.com>
2021-07-12 19:44:43 +0000

    Resolved typos in exos 7 and 14

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,3 @@
--- Computes the score average of all records in the table second_table
-SELECT AVG(score) as average from second_table
+-- Computes the score average of all records
+-- in the table second_table
+SELECT AVG(score) AS average FROM second_table
