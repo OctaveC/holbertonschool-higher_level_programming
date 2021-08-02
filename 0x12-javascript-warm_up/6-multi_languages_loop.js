374a924ba46d711284cce3463772416663e49b9d
OctaveC <octavec@outlook.com>
2021-08-02 21:16:02 +0000

    weird issues with exo 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 
-const langu = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-langu.forEach(element => console.log(element));
+const sentences = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+sentences.forEach(element => console.log(element));
