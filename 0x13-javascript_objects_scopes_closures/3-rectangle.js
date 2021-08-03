99aaf73621e5845f873510056229f16565650840
OctaveC <octavec@outlook.com>
2021-08-03 12:10:36 +0000

    first batch of exos, creating our rectangle object

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let ite = 0; ite < this.height; ite++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,1 @@
+This is abouit some more javascript
