35c52f531fd96dc9fb2fe89877e53fda2abf5eb2
octave collombel <octavec@outlook.com>
2021-08-19 20:18:53 +0200

    problem exo 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept.
-curl -Is "$1" -X OPTIONS | grep "Allow:" | sed 'Allow: ||'
+curl -Is "$1" | grep "Allow:" | sed "s|Allow: ||"
