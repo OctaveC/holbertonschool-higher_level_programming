edd204f418f342703618a5a0c1c7105d677d9443
octave collombel <octavec@outlook.com>
2021-08-19 19:59:33 +0200

    fixed the three lines requirement

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends a request to a URL passed as an argument,
-# and displays only the status code of the response.
+# Sends a request to a URL passed as an argument, and displays only the status code of the response.
 curl -swo /dev/null "%{http_code}" "$1"
