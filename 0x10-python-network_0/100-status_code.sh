689fac37428ce866d3eac0bb6ad255b96a165206
octave collombel <octavec@outlook.com>
2021-08-19 20:22:00 +0200

    problem exo 100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -swo /dev/null "%{http_code}" "$1"
+curl -o /dev/null -sw "%{http_code}" "$1"
